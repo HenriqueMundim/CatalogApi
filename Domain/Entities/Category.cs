de7ce8161311fca45f1b816c0f9c6a10017927af
Henrique Matos <henriquemundimmatos@gmail.com>
2024-09-19 14:38:47 -0300

    feat: Data annotations

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CatalogApi.Domain.Entities
 {
@@ -7,10 +8,19 @@ namespace CatalogApi.Domain.Entities
         public Category() {
             Products = new Collection<Product>();
         }
+
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string? Name { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImageUrl { get; set; }
 
+        [Required]
         public ICollection<Product>? Products { get; set; }
     }
 }
--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -1,20 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CatalogApi.Domain.Entities
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(80)]
         public string? Name { get; set; }
 
+        [Required]
+        [StringLength(300)]
         public string? Description { get; set; }
 
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal? Price { get; set; }
 
+        [Required]
+        [StringLength(300)]
         public string? ImageUrl { get; set; }
 
+        [Required]
         public float? Stock { get; set; }
 
+        [Required]
         public int CategoryId { get; set; }
+
         public Category? Category { get; set; }
     }
 }
